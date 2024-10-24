4b1a1c2f163fe870312ad17d1e6c599c7f436ce9
Eduardo Kraus <kraus@eduardokraus.com>
2024-10-24 17:44:21 -0300

    V 1.0.21

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2024102400;
+$plugin->version   = 2024102401;
 $plugin->requires  = 2014051220;
-$plugin->release   = '1.0.20';
+$plugin->release   = '1.0.21';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'local_geniai';
