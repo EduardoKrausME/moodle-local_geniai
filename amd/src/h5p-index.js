e5d05ce9181f0655a4b1c3ff63380b63491d4cb5
Eduardo Kraus <kraus@eduardokraus.com>
2025-02-06 22:30:14 -0300

    Index

--- amd/src/h5p-index.js
+++ amd/src/h5p-index.js
@@ -1,47 +1,48 @@
 define(["jquery", "core/modal_factory"], function($, ModalFactory) {
-    var index = {
+    return {
+
         readmore: function() {
+
+            function createReadMore($lement) {
+                const fullText = $lement.text();
+                console.trace(fullText);
+                $lement.attr("data-fulltext", fullText);
+                const maxLength = $lement.attr("data-size");
+
+                // Se o texto for maior que o limite, aplica a funcionalidade "Read More"
+                if (fullText.length > maxLength) {
+                    var partes = fullText.split(" ");
+                    var truncatedText = "";
+                    for (var i = 0; i < partes.length; i++) {
+                        if (`${truncatedText} ${partes[i]}`.length > maxLength) {
+                            break;
+                        }
+                        truncatedText = `${truncatedText} ${partes[i]}`;
+                    }
+
+                    var readmore = `<span class="btn btn-link readmore-link">${M.util.get_string("h5p-readmore", "local_geniai")}</span>`;
+                    $lement.html(truncatedText + readmore);
+                    $lement.find(".readmore-link").click(function() {
+                        console.log($lement.attr("data-fulltext"));
+                        $lement.html($lement.attr("data-fulltext"));
+                    });
+                }
+            }
+
             $(".text-readmore").each(function() {
-                index.createReadMore($(this));
+                createReadMore($(this));
             });
 
-            $("#h5p-create-new").click(function() {
+            $("#h5p-create-new-link").click(function() {
                 ModalFactory.create({
                     type: ModalFactory.types.DEFAULT,
-                    title: M.util.get_string('h5p-page-title', "local_geniai"),
+                    title: M.util.get_string("h5p-page-title", "local_geniai"),
                     body: $("#h5p-body-create"),
                     footer: "",
                 }).done(function(modal) {
                     modal.show();
                 })
             });
-        },
-
-        createReadMore: function($lement) {
-            var fullText = $lement.text();
-            var maxLength = $lement.attr("data-size");
-
-            // Se o texto for maior que o limite, aplica a funcionalidade "Read More"
-            if (fullText.length > maxLength) {
-                var partes = fullText.split(" ");
-                var truncatedText = "";
-                for (var i = 0; i < partes.length; i++) {
-                    if (`${truncatedText} ${partes[i]}`.length > maxLength) {
-                        break;
-                    }
-                    truncatedText = `${truncatedText} ${partes[i]}`;
-                }
-
-                $lement.text(truncatedText);
-                var readmore = $(`<a href="javascript:void(0);" class="readmore-link">${M.util.get_string('h5p-readmore', "local_geniai")}</a>`);
-                $lement.append(readmore)
-                    .append("<br>")
-                    .click(function() {
-                        $lement.text(fullText);
-                    });
-            }
         }
-    };
-
-    return index;
+    }
 });
