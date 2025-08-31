6e3d5c1b5d69dfe4444ca286169e5b2823dcd9b4
Eduardo Kraus <kraus@eduardokraus.com>
2025-08-31 10:19:53 -0300

    Add pluginfile

--- lib.php
+++ lib.php
@@ -35,3 +35,41 @@ function local_geniai_before_footer() {
     core_hook_output::before_footer_html_generation();
 }
 
+/**
+ * Serve the files from the geniai file areas
+ *
+ * @param stdClass $course    the course object
+ * @param stdClass $cm        the course module object
+ * @param context $context    the context
+ * @param string $filearea    the name of the file area
+ * @param array $args         extra arguments (itemid, path)
+ * @param bool $forcedownload whether or not force download
+ * @param array $options      additional options affecting the file serving
+ *
+ * @return bool false if the file not found, just send the file otherwise and do not return anything
+ * @throws Exception
+ */
+function local_geniai_pluginfile($course, $cm, context $context, $filearea, $args, $forcedownload, array $options = []) {
+
+    require_login($course, true, $cm);
+    $itemid = array_shift($args);
+
+    // Extract the filename / filepath from the $args array.
+    $filename = array_pop($args); // The last item in the $args array.
+    if (!$args) {
+        // Variable $args is empty => the path is "/".
+        $filepath = "/";
+    } else {
+        // Variable $args contains elements of the filepath.
+        $filepath = "/" . implode("/", $args) . "/";
+    }
+
+    // Retrieve the file from the Files API.
+    $fs = get_file_storage();
+    $file = $fs->get_file($context->id, "local_geniai", $filearea, $itemid, $filepath, $filename);
+    if ($file) {
+        send_stored_file($file, 86400, 0, $forcedownload, $options);
+        return true;
+    }
+    return false;
+}
