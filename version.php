64547c05300100a79f982566a99cdfee11920e0e
Eduardo Kraus <kraus@eduardokraus.com>
2026-07-06 20:26:11 -0300

    'MOODLE_INTERNAL'

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined("MOODLE_INTERNAL") || die;
+defined('MOODLE_INTERNAL') || die;
 
 $plugin->version = 2026070600;
 $plugin->release = "2.4.0";
