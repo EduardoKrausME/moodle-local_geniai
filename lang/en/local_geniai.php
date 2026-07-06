b95a4c8efb7abf9b3481e731682af6dd6a1807d6
Eduardo Kraus <kraus@eduardokraus.com>
2026-07-06 20:22:51 -0300

    The use of backticks in strings is not recommended

--- lang/en/local_geniai.php
+++ lang/en/local_geniai.php
@@ -15,6 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * phpcs:disable moodle.Strings.ForbiddenStrings.Found
+ *
  * lang pt_br file.
  *
  * @package   local_geniai
--- lang/uk/local_geniai.php
+++ lang/uk/local_geniai.php
@@ -15,6 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * phpcs:disable moodle.Strings.ForbiddenStrings.Found
+ *
  * lang uk file.
  *
  * @package   local_geniai
