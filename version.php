e4f5127d3851a6dce8616f7b17bd49270a533a53
Eduardo Kraus <kraus@eduardokraus.com>
2026-07-10 08:59:38 -0300

    V 3.0.1

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2026070600;
-$plugin->release = "3.0.0";
+$plugin->version = 2026071000;
+$plugin->release = "3.0.1";
 $plugin->requires = 2020110900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = "local_geniai";
